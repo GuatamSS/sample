60731f826cc29b1e065629edbe38fffac6f2d0dc
Gautam <1freehosting1@gmail.com>
2017-10-23 01:56:12 +0530

    modified ftp push

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#git config git-ftp.user frore-mitten
-#git config git-ftp.url files.000webhost.com/public_html/
-#git config git-ftp.password hellohello
+git config git-ftp.user frore-mitten
+git config git-ftp.url files.000webhost.com/public_html/
+git config git-ftp.password hellohello
 #git ftp init --user $Username --passwd $Pwd ftp://domain.com/public_html/
 git ftp push
